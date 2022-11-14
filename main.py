6c280a80041491bedeeab41ef2ff606ebd61c13e
Anshika Bhasin <41538257+anshika109@users.noreply.github.com>
2022-11-14 12:49:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ print("hello world")
 
 def welcome():
   
-  
-  print("welcome to github actions workflow testing")
-  
-  
+    print("welcome to github actions workflow testing")
+    
+
 print("Tutorial for github actions")
 welcome()
